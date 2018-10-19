d583854e0c884d224e3c9eb2d9dc177957989c9f
Sema <sema@pyva.net>
2018-10-19 01:09:15 -0400

    Wrapped for npm

--- src/webaudio-tinysynth.js
+++ src/webaudio-tinysynth.js
@@ -1,4 +1,19 @@
 "use strict";
+(function(global, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory();
+  }
+  else if (typeof define === 'function' && define.amd) {
+    define('WebAudioTinySynth', [], factory);
+  }
+  else {
+    global.WebAudioTinySynth = factory();
+  }
+})(this, function() {
+
+// Please don't remove this comment!
+// START WebAudioTinySynth()
+
 function WebAudioTinySynth(opt){
   this.__proto__ = this.sy =
   /*include ../tmp/webaudio-tinysynth-nogui.js*/
@@ -16,3 +31,9 @@ function WebAudioTinySynth(opt){
   }
   this.ready();
 }
+
+// END WebAudioTinySynth()
+// Please don't remove this comment!
+
+  return WebAudioTinySynth;
+});
